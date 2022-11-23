ac53b7753046f0de1d3006fdc153c17de0871c56
khediril <khediril@yahoo.fr>
2022-11-23 11:38:31 +0100

    ajout relation entre produit et categorie

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Produit;
+use App\Entity\Categorie;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -10,11 +11,16 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $categ = new Categorie();
+        $categ->setNom("categ1");
+        $manager->persist($categ);
+        //$manager->flush();
         for ($i=0; $i <20 ; $i++) { 
             $produit = new Produit();
-        $produit->setNom("Produit"+$i);
+        $produit->setNom("Produit".$i);
         $produit->setPrix(mt_rand(1000, 5000));
         $produit->setQuantite(mt_rand(1, 100));
+        $produit->setCategorie($categ);
         $manager->persist($produit);
         }
         $manager->flush();
--- src/Entity/Produit.php
+++ src/Entity/Produit.php
@@ -37,6 +37,12 @@ class Produit
      */
     private $descrition;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=Categorie::class, inversedBy="produits")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $categorie;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -86,6 +92,18 @@ class Produit
     public function setDescrition(?string $descrition): self
     {
         $this->descrition = $descrition;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?Categorie
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(?Categorie $categorie): self
+    {
+        $this->categorie = $categorie;
 
         return $this;
     }
